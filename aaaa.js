4eb612a8507805cfb9a6caa7e95e2935d85839e6
gomorita <taopaigyo@yahoo.co.jp>
2021-01-17 23:10:05 +0900

    学習２まで

--- aaaa.js
+++ aaaa.js
@@ -180,3 +180,149 @@ console.log(number);
 number += 1;
 console.log(number);
 
+// 変数numberを定義してください
+let numberman=1;
+
+// while文を作成してください
+while (numberman<=100){
+  console.log(numberman);
+  numberman+=1;
+}
+
+// for文を用いて、1から100までの数字を出力してください
+for(let numbermen=1;numbermen<=100;numbermen++){
+  console.log(numbermen);
+}
+
+// for文を完成させてください
+for (let numbermon=1;numbermon<=100;numbermon++) {
+
+  
+  // if文を用いて、numberが3の倍数の時に「3の倍数です」、そうでないときは数字を出力してください
+  if(numbermon%3===0){
+    console.log("3の倍数です");
+  }else{
+    console.log(numbermon);
+  }
+}
+
+
+// 定数animalsに、指定された配列を代入してください
+const animals = ["dog", "cat", "sheep", "rabbit", "monkey", "tiger", "bear", "elephant"];
+
+// 定数animalsを出力して下さい
+console.log(animals);
+
+
+// 配列の1つ目の要素を出力してください
+console.log(animals[0]);
+
+// 配列の3つ目の要素を出力してください
+console.log(animals[2]);
+
+// 配列animalsの3つ目の要素を「rabbit」に更新してください
+animals[2]="rabbit";
+
+// 配列animalsの3つ目の要素をコンソールに表示して下さい
+console.log(animals[2]);
+
+for(let i =0;i<3;i++){
+  console.log(animals[i]);
+}
+
+
+// lengthを用いて配列の要素の数を出力してください
+console.log(animals.length);
+
+// lengthを用いて条件式を書き換えてください
+for (let i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
+}
+
+// 定数characterを定義し、指定されたオブジェクトを代入してください
+const character = [
+  {name: "にんじゃわんこ", age: 14},
+  {name: "ひつじ仙人", age: 1000}
+];
+
+
+// characterの値を出力してください
+console.log(character[0]);
+
+// characterのnameの値を出力してください
+console.log(character[0].name);
+
+// characterのageの値を「20」に更新してください
+character[0].age=20;
+
+// characterをコンソールに出力してください
+console.log(character[0]);
+
+
+
+// charactersの1つ目の要素をコンソールに出力してください
+console.log(character[0]);
+
+// charactersの2つ目の要素の「name」に対応する値をコンソールに出力してください
+console.log(character[1].name);
+
+
+const characters = [
+  {name: "にんじゃわんこ", age: 14},
+  {name: "ひつじ仙人", age: 100},
+  {name: "ベイビーわんこ", age: 5},
+  {name: "とりずきん"}
+];
+
+
+// for文を完成させてください
+for (let i=0;i<characters.length;i++) {
+  console.log("--------------------");
+  
+  // 定数characterを定義してください
+  const character=characters[i];
+  
+  // 「名前は〇〇です」を出力してください
+  console.log(`名前は${character.name}です`);
+  
+  // 「〇〇歳です」を出力してください
+  console.log(`${character.age}歳です`);
+  
+}
+
+for (let i = 0; i < characters.length; i++) {
+  console.log("--------------------");
+  
+  const character = characters[i];
+  
+  console.log(`名前は${character.name}です`);
+  
+  // if文を追加してください
+  if(character.age===undefined){
+  console.log("年齢は秘密です");
+  }else{
+    console.log(`${character.age}歳です`);
+  }
+  
+}
+
+const cafe = {
+  name: "Progateカフェ",
+  businessHours: { 
+    opening: "10:00(AM)",
+    closing: "8:00(PM)"
+  },
+  // menusプロパティに配列を代入してください
+  menus:["コーヒー","紅茶","チョコレートケーキ"]
+  
+};
+
+console.log(`店名: ${cafe.name}`);
+console.log(`営業時間:${cafe.businessHours.opening}から${cafe.businessHours.closing}`);
+console.log(`----------------------------`);
+console.log("おすすめメニューはこちら");
+
+// for文を用いて配列menusの中身を表示させてください
+for(let i=0;i<cafe.menus.length;i++){
+  console.log(cafe.menus[i]);
+}
