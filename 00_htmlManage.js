912e722ebcb8dca236165b1a684e2e95f3d365a6
aa576611 <takeru.davis@ibm.com>
2021-01-30 12:12:16 +0900

    02画面作成途中

---
+++ 00_htmlManage.js
@@ -0,0 +1,10 @@
+//htmlとの受け渡しを司る処理全般のファイル
+
+//02画面用の処理
+
+
+//ホストネームを受け取る
+var get_hostName = document.getElementById("hostNameImput").value; //htmlからホストネームの入力内容を取得
+//    document.getElementById("output_message").innerHTML = get_hostName;
+console.log(get_hostName);
+
