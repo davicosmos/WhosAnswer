161a058ac290e6b04ff68205a233502c40e38486
aa576611 <takeru.davis@ibm.com>
2021-04-25 12:47:07 +0900

    出題されるクイズが切り替わらない問題あり

--- js-quiz-apps-master/app.js
+++ js-quiz-apps-master/app.js
@@ -12,15 +12,50 @@ const quizLen = quiz.length;
 //1.ランダム数字の上限を指定(0〜指定した数字の範囲)
 let maxNumRange = quizLen;
 
+
+
 //2.ランダムな数字を生成
-let randomNum = parseInt(Math.random() * maxNumRange) + 1;
+//let randomNum = parseInt(Math.random() * maxNumRange) + 1;
 
-//3.ランダム生成された数字の検証
-console.log(randomNum);
+/** 重複チェック用配列 */
+let randoms = [];
+/** 最小値と最大値 */
+let min = 0, max = quizLen-1;
+ 
+/** 重複チェックしながら乱数作成 */
+for(i = min; i <= max; i++){
+  while(true){
+    let tmp = randomNum(min, max);
+    if(!randoms.includes(tmp)){
+      randoms.push(tmp);
+      break;
+    }
+  }
+}
+ 
+/** min以上max以下の整数値の乱数を返す */
+function randomNum(min, max){
+  return Math.floor( Math.random() * (max - min + 1)) + min;
+}
 
-let quizCount = 0;
+
+
+/* for(let i = 0; i < randoms.length; i++){
+  randoms[i];
+  console.log(randoms[i]);
+  };
+ */
+console.log(randoms);
+randomNum();
+
+
+//1問目のクイズの配列番号
+let syutudaiJun = 0;
+let quizCount = randoms[syutudaiJun];
+//クイズの正解数
 let score = 0;
 
+//html上の出題枠に記載されているテキストをクイズ問題文で上書く(=出題する)
 const init = () => {
   $question.textContent = quiz[quizCount].question;
   
@@ -33,12 +68,13 @@ const init = () => {
   }
 };
 
+//クイズ数があるだけ出題
 const goToNext = () => {
-  quizCount++;
-  if(quizCount < quizLen){
+  syutudaiJun++;
+  if(syutudaiJun < quizLen){
     init(quizCount);
   } else {
-    // $window.alert('クイズ終了！');
+    $window.alert('クイズ終了！');
     showEnd();
   }
 };
